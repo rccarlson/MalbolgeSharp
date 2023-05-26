57162e57aef073b63f022b1810ded4ad139595db
Riley Carlson <riley.carlson@ksb.com>
2023-05-26 16:57:19 -0400

    Add profiling function

--- Malbolge/Program.cs
+++ Malbolge/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Malbolge;
 static void PrintQueue(Queue<char> queue) { while (queue.TryDequeue(out var output)) { Console.Write(output); } Console.WriteLine(); }
 static void PopulateQueue(Queue<char> queue, string str) { foreach (var c in str) queue.Enqueue(c); }
-static string SimpleExecute(MalbolgeFlavor flavor, string program)
+
+static double Profile(int iterations, Action action)
 {
-	var vm = new VirtualMachine(flavor, program);
-	var report = vm.Execute();
-	Console.WriteLine(report.Result);
-	return report.Result;
+	var sw = System.Diagnostics.Stopwatch.StartNew();
+	for (int i = 0; i < iterations; i++)
+		action();
+	sw.Stop();
+	return sw.Elapsed.TotalMicroseconds / iterations;
 }
 
 var helloWorldProgram = """
@@ -31,3 +33,11 @@ var helloWorldProgram2 = """
 (=<`#9]~6ZY327Uv4-QsqpMn&+Ij"'E%e{Ab~w=_:]Kw%o44Uqp0/Q?xNvL:`H%c#DD2^WV>gY;dts76qKJImZkj
 """; // https://gist.github.com/kspalaiologos/a1fe6913aaff8edea515b4af385368fe
 VirtualMachine.Execute(MalbolgeFlavor.Specification, helloWorldProgram2);
+
+VirtualMachine.Execute(MalbolgeFlavor.Implementation, helloWorldProgram);
+
+var time = Profile(1_000, () =>
+{
+	VirtualMachine.Execute(MalbolgeFlavor.Implementation, helloWorldProgram);
+});
+Console.WriteLine(time);
